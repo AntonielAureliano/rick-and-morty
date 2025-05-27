d73891ddfb16814dd914b981ecb86a5dc4b9e361
Antoniel Aureliano <tonielaureliano@gmailcom>
2025-05-27 17:24:59 -0300

    feat(CharacterList): add CharacterList component.

---
+++ src/components/CharacterList/index.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { CharactersCard } from "../CharactersCard";
+
+import styles from "./CharacterList.module.css";
+
+export const CharacterList = () => {
+    const [characters, setCharacters] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const [info, setInfo] = useState({});
+
+    useEffect(() => {
+        setLoading(true);
+        axios.get(`https://rickandmortyapi.com/api/character/?page=${page}`)
+        .then(response => {
+            setCharacters(response.data.results);
+            setInfo(response.data.info);
+            setLoading(false);
+        })
+        .catch(error => {
+            console.error(error);
+            setLoading(false);
+        });
+    }, [page]);
+
+    if(loading) {
+            return <div>Loading...</div>
+    }
+
+    return (
+        <div>
+            <div className={styles.charactersGrid}>
+                {characters.map(character => (
+                    <CharactersCard key={character.id} character={character} />
+                ))}
+            </div>
+        </div>
+    )
+}
